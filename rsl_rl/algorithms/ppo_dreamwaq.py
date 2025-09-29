6e8e44435cb67a5e3f35065a2a2033b1370a16ac
Carroll <carroll511@g.skku.edu>
2025-09-29 17:00:00 +0900

    save

--- rsl_rl/algorithms/ppo_dreamwaq.py
+++ rsl_rl/algorithms/ppo_dreamwaq.py
@@ -65,7 +65,7 @@ class PPODreamWaQ:
     def train_mode(self):
         self.actor_critic.train()
 
-    def act(self, obs, critic_obs, history_obs):
+    def act(self, obs, critic_obs, history_obs, velocity_targets):
     # def act(self, obs, critic_obs, history_obs):
         if self.actor_critic.is_recurrent:
             self.transition.hidden_states = self.actor_critic.get_hidden_states()
@@ -79,6 +79,7 @@ class PPODreamWaQ:
         self.transition.observations = obs
         self.transition.history_observations = history_obs
         self.transition.critic_observations = critic_obs
+        self.transition.velocity_targets = velocity_targets
         return self.transition.actions
     
     def process_env_step(self, rewards, dones, infos):
@@ -112,7 +113,7 @@ class PPODreamWaQ:
         else:
             generator = self.storage.mini_batch_generator(self.num_mini_batches, self.num_learning_epochs)
         for obs_batch, critic_obs_batch, history_obs_batch, actions_batch, target_values_batch, advantages_batch, returns_batch, old_actions_log_prob_batch, \
-            old_mu_batch, old_sigma_batch, hid_states_batch, masks_batch in generator:
+            old_mu_batch, old_sigma_batch, hid_states_batch, masks_batch, velocity_targets_batch, in generator:
             
                 # predicted_velocity_batch, _, reconstructed_next_obs_batch, latent_mu, logvar = self.actor_critic.forward(history_obs_batch)
 
@@ -160,7 +161,7 @@ class PPODreamWaQ:
                     value_loss = (returns_batch - value_batch).pow(2).mean()
 
                 # CENet loss                
-                velocity_loss = nn.MSELoss()(predicted_velocity_batch, critic_obs_batch[:, 45:48])
+                velocity_loss = nn.MSELoss()(predicted_velocity_batch, velocity_targets_batch)
                 recon_loss = nn.MSELoss()(reconstructed_obs_batch, critic_obs_batch[:, :45])
                 kl_loss = -0.5 * torch.mean(1 + logvar - latent_mu.pow(2) - logvar.exp())
                 ce_loss = velocity_loss + 0.1*(recon_loss + kl_loss * self.beta_coef)
--- rsl_rl/storage/rollout_storage.py
+++ rsl_rl/storage/rollout_storage.py
@@ -49,6 +49,7 @@ class RolloutStorage:
             self.action_mean = None
             self.action_sigma = None
             self.hidden_states = None
+            self.velocity_targets = None
         
         def clear(self):
             self.__init__()
@@ -88,6 +89,8 @@ class RolloutStorage:
         self.saved_hidden_states_a = None
         self.saved_hidden_states_c = None
 
+        self.velocity_targets = torch.zeros(num_transitions_per_env, num_envs, 3, device=self.device)
+
         self.step = 0
 
     def add_transitions(self, transition: Transition):
@@ -103,6 +106,7 @@ class RolloutStorage:
         self.actions_log_prob[self.step].copy_(transition.actions_log_prob.view(-1, 1))
         self.mu[self.step].copy_(transition.action_mean)
         self.sigma[self.step].copy_(transition.action_sigma)
+        self.velocity_targets[self.step].copy_(transition.velocity_targets)
         self._save_hidden_states(transition.hidden_states)
         self.step += 1
 
@@ -172,6 +176,8 @@ class RolloutStorage:
         old_mu = self.mu.flatten(0, 1)
         old_sigma = self.sigma.flatten(0, 1)
 
+        velocity_targets = self.velocity_targets.flatten(0, 1)
+
 
         for epoch in range(num_epochs):
             for i in range(num_mini_batches):
@@ -191,10 +197,11 @@ class RolloutStorage:
                 advantages_batch = advantages[batch_idx]
                 old_mu_batch = old_mu[batch_idx]
                 old_sigma_batch = old_sigma[batch_idx]
+                velocity_targets_batch = velocity_targets[batch_idx]
                 # next_obs_batch = next_observations[batch_idx]
 
                 yield obs_batch, critic_observations_batch, history_observations_batch, actions_batch, target_values_batch, advantages_batch, returns_batch, \
-                       old_actions_log_prob_batch, old_mu_batch, old_sigma_batch, (None, None), None
+                       old_actions_log_prob_batch, old_mu_batch, old_sigma_batch, velocity_targets_batch, (None, None), None
 
     # for RNNs only
     def reccurent_mini_batch_generator(self, num_mini_batches, num_epochs=8):

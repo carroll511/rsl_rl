89386c7522aceb40ab3d1243abe89a636b858a62
Carroll <carroll511@g.skku.edu>
2025-09-29 16:52:43 +0900

    save

--- rsl_rl/runners/on_policy_runner_dreamwaq.py
+++ rsl_rl/runners/on_policy_runner_dreamwaq.py
@@ -56,7 +56,7 @@ class OnPolicyRunnerDreamWaQ:
         # wandb init
         wandb.init(
             project="leggedgym_project",
-            name=self.cfg.get("exp_name", "a1_dreamwaq_env_4096"),
+            name=self.cfg.get("exp_name", "a1_dreamwaq_vae_half"),
             config=train_cfg
         )
 

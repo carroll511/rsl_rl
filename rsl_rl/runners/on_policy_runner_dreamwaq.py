1787218de35ce7bff530d5a69cdc9bd20ece542b
Carroll <carroll511@kaist.ac.kr>
2025-09-30 18:00:13 +0900

    added domain randomization

--- rsl_rl/runners/on_policy_runner_dreamwaq.py
+++ rsl_rl/runners/on_policy_runner_dreamwaq.py
@@ -56,7 +56,7 @@ class OnPolicyRunnerDreamWaQ:
         # wandb init
         wandb.init(
             project="leggedgym_project",
-            name=self.cfg.get("exp_name", "a1_dreamwaq_vel_targets"),
+            name=self.cfg.get("exp_name", "a1_dreamwaq_vel_targets_domain_random"),
             config=train_cfg
         )
 

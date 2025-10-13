a161b3b5ce76cc221b4c44227913b889595406d4
carroll <carroll@kaist.ac.kr>
2025-10-13 20:20:15 +0900

    save

--- rsl_rl/runners/on_policy_runner_dreamwaq.py
+++ rsl_rl/runners/on_policy_runner_dreamwaq.py
@@ -56,7 +56,7 @@ class OnPolicyRunnerDreamWaQ:
         # wandb init
         wandb.init(
             project="leggedgym_project",
-            name=self.cfg.get("exp_name", "a1_dreamwaq_vel_targets_noise_vec"),
+            name=self.cfg.get("exp_name", "a1_dreamwaq_vel_targets_test"),
             config=train_cfg
         )
 

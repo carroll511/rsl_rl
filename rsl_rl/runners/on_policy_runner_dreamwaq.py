efe581e3b7bbf30383ee379d5beff4239175bd5a
carroll <carroll@kaist.ac.kr>
2025-11-03 15:55:18 +0900

    grid curriculum

--- rsl_rl/runners/on_policy_runner_dreamwaq.py
+++ rsl_rl/runners/on_policy_runner_dreamwaq.py
@@ -56,7 +56,7 @@ class OnPolicyRunnerDreamWaQ:
         # wandb init
         wandb.init(
             project="leggedgym_project",
-            name=self.cfg.get("exp_name", "a1_dreamwaq_adaptive"),
+            name=self.cfg.get("exp_name", "a1_dreamwaq_grid_currciulum"),
             config=train_cfg
         )
 
